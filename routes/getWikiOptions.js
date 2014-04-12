c80f3409069be3a157c49c6fe7a4575a9354384e
Shrikanth Narayanan <shrikanth2@gmail.com>
2014-04-12 12:58:31 -0700

    added bare project files

---
+++ routes/getWikiOptions.js
@@ -0,0 +1,34 @@
+var http = require("http");
+
+exports.getJSON = function(req, res)
+{
+    
+    console.log('dsdsds ddsdjk');
+
+    var options = {
+      host: 'en.wikipedia.org',
+      path: '/w/api.php?action=opensearch&format=json&search=usc&namespace=0&limit=100&suggest='
+    };
+
+    callback = function(response) {
+      var str = '';
+
+      //another chunk of data has been recieved, so append it to `str`
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+
+      //the whole response has been recieved, so we just print it out here
+      response.on('end', function () {
+        console.log(str);
+        res.set('content-type', 'application/json');
+        res.send(str);
+      });
+    }
+
+    http.request(options, callback).end();
+
+
+
+    //res.send('dsdsds ddsdjk');
+};
