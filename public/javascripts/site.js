4d2f583e4ce26ff917a9f49ec68182247c2a7cf0
Shrikanth Narayanan <shrikanth2@gmail.com>
2014-04-12 16:49:12 -0700

    adding bootstrap lib

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -12,6 +12,17 @@ $(document).ready(function(){
           });
       },
       select: function( event, ui ) {
+
+        $.ajax( {
+            url : "/getWikiPage?term=" + ui.item.value, 
+            success : function(data) { 
+              //$('div.container-fluid').append("<div class='row' id='divWikiResults'><div class='col-xs-8'><iframe id='divWikiPage' style='display: block' style='width:100%'></iframe></div> <div class='col-xs-4'>here is the you tube</div></div>");
+              $('#divWikiPage').attr('src',data);
+              //$('#divWikiResults').slideDown(1000);
+              //$('#divWikiPage').html(data); 
+            }
+          });
+
         $( "#txtSearch" ).val( ui.item.value );
         return false;
       }
