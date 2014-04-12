99286c678da51c6fc9ce8369a769b1d35d8d8179
Shrikanth Narayanan <shrikanth2@gmail.com>
2014-04-12 14:15:02 -0700

    updated getWikiOptions file

--- routes/getWikiOptions.js
+++ routes/getWikiOptions.js
@@ -1,34 +1,55 @@
 var http = require("http");
+var url = require('url');
+
 
 exports.getJSON = function(req, res)
 {
-    
-    console.log('dsdsds ddsdjk');
+  var options = {
+    host: 'en.wikipedia.org',
+    path: '/w/api.php?action=opensearch&format=json&search='+req.query.term+'&namespace=0&limit=100&suggest='
+  };
 
-    var options = {
-      host: 'en.wikipedia.org',
-      path: '/w/api.php?action=opensearch&format=json&search=usc&namespace=0&limit=100&suggest='
-    };
+  callback = function(response) {
+    var str = '';
 
-    callback = function(response) {
-      var str = '';
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
 
-      //another chunk of data has been recieved, so append it to `str`
-      response.on('data', function (chunk) {
-        str += chunk;
-      });
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+      console.log(str);
+      res.set('content-type', 'application/json');
+      res.send(str);
+    });
+  }
 
-      //the whole response has been recieved, so we just print it out here
-      response.on('end', function () {
-        console.log(str);
-        res.set('content-type', 'application/json');
-        res.send(str);
-      });
-    }
+  http.request(options, callback).end();
+};
 
-    http.request(options, callback).end();
+exports.getWikiPage = function(req, res)
+{
+  var options = {
+    host: 'en.wikipedia.org',
+    path: '/w/api.php?action=opensearch&format=json&search='+req.query.term+'&namespace=0&limit=100&suggest='
+  };
 
+  callback = function(response) {
+    var str = '';
 
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
 
-    //res.send('dsdsds ddsdjk');
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+      console.log(str);
+      res.set('content-type', 'application/json');
+      res.send(str);
+    });
+  }
+
+  http.request(options, callback).end();
 };
