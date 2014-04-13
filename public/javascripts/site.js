ebaff75082379699cd6a7e7e2e93ca4e4bcaf2f0
Shrikanth Narayanan <shrikanth2@gmail.com>
2014-04-13 02:22:46 -0700

    Implemented reneding ofr videos and playback added toggling search bar after results are shown

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+  $('#topMenu').bind('click', function(evt){
+    $('#divSearchTool').slideDown(500);
+    $('#topMenu').hide();
+  });
+
 	$( "#txtSearch" ).autocomplete({
       minLength: 2,
       source: function( request, response ) 
@@ -17,15 +22,25 @@ $(document).ready(function(){
             url : "/getEntities?term=" + ui.item.value, 
             success : function(data) { 
             if(data['items']) {
-            var items = data['items'];
-            var content = '';
-            for(var i in items)  {
-                content += "<div class='media'><a class='pull-left' href='#'> \
-                <img src='"+items[i]['snippet']['thumbnails']['default']['url']+"' alt='"+items[i]['snippet']['title']+"' class='img-thumbnail' /> \
-                </a> <div class='media-body'> <h4 class='media-heading'>"+items[i]['snippet']['title']+"</h4> "+items[i]['snippet']['description']+"</div></div>";
+              var items = data['items'];
+              var content = '';
+              for(var i in items)  {
+                  content += "<div class='media'><a class='pull-left' href='#'> \
+                  <img src='"+items[i]['snippet']['thumbnails']['default']['url']+"' alt='"+items[i]['snippet']['title']+"' class='img-thumbnail' /> \
+                  </a> <div class='media-body'> <h4 class='media-heading'><a class='youtube' rel='"+items[i]['id']['videoId']+"' href='javascript::void(0)'>"+items[i]['snippet']['title']+"</a></h4> "+items[i]['snippet']['description']+"</div></div>";
+              };
+              $('#video_playlist').html(content);
+
+              $('a.youtube',$('#video_playlist')).click('bind',function(evt){
+                $('#youtube_video').html('<iframe id="ytplayer" type="text/html" width="425" height="349" src="http://www.youtube.com/embed/'+$(this).attr('rel')+'?autoplay=1&origin=http://example.com" frameborder="0"/>');                
+                $("html, body").animate({ scrollTop: 0 }, "slow");          
+                // $('#ytplayer').attr('src', "http://www.youtube.com/embed/"+$(this).attr('rel')+"?autoplay=1&origin=http://example.com");
+              });
+
+              $('#youtube_video').slideDown(1000);
+              $('#divSearchTool').slideUp(1000);
+              $('#topMenu').show();
             };
-            $('#video_playlist').html(content);
-            }
           }
         });
 
@@ -71,3 +86,7 @@ $(document).ready(function(){
 
   
 });
+
+function playVideo() {
+
+};
