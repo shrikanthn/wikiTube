fd5d71b08eee266376d459140c5954704c87fa75
Shrikanth Narayanan <shrikanth2@gmail.com>
2014-04-12 18:45:36 -0700

    implemented querying google youtube api

--- routes/getWikiOptions.js
+++ routes/getWikiOptions.js
@@ -1,5 +1,6 @@
 // var http = require('follow-redirects').http; // 
 var http = require("http");
+var https = require("https");
 var url = require('url');
 
 
@@ -66,6 +67,61 @@ exports.getWikiPage = function(req, res)
       //   }
       // );
 
+
+      var newUrl = response.headers.location;
+      res.send(newUrl);
+    });
+  }
+
+  http.request(options, callback).end();
+};
+
+
+
+
+exports.searchYoutube = function(req, res)
+{
+  var options = {
+    host: 'www.googleapis.com',
+    path: '/youtube/v3/search?part=snippet&maxResults=20&order=viewCount&key=AIzaSyBgz_iSlDmVzMW2dhaNwnV9oFWjDFHDLio&q='+encodeURIComponent(req.body.term)
+  };
+
+  callback = function(response) {
+    var str = '';
+
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    response.on('end', function () {
+      console.log(str);
+      res.set('content-type', 'application/json');
+      res.send(str);
+    });
+  }
+
+  https.request(options, callback).end();
+};
+
+exports.getYoutubeVideos = function(keywords)
+{
+  var options = {
+    host: 'www.googleapis.com',
+    path: '/youtube/v3/search?part=snippet&maxResults=10&order=viewCount&key=AIzaSyBgz_iSlDmVzMW2dhaNwnV9oFWjDFHDLio&key='+encodeURIComponent(keywords)
+  };
+
+  callback = function(response) {
+    var str = '';
+
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+      console.log(str);
+      console.log(response.headers);
 
       var newUrl = response.headers.location;
       res.send(newUrl);
